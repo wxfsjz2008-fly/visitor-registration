f0ad96a35eac64d27f26cd6429738284da6e824b
wxfsjz2008-fly <wxfsjz2008@gmail.com>
2026-05-17 17:06:05 +0800

    refactor: use React.lazy for route-level code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
-import { RegistrationPage, ListPage } from './pages'
 import { Toaster } from './components/ui/toaster'
 import './index.css'
+
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage').then(m => ({ default: m.RegistrationPage })))
+const ListPage = lazy(() => import('./pages/ListPage').then(m => ({ default: m.ListPage })))
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<RegistrationPage />} />
-          <Route path="list" element={<ListPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div className="flex items-center justify-center min-h-screen">加载中...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<RegistrationPage />} />
+            <Route path="list" element={<ListPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
       <Toaster />
     </BrowserRouter>
   )
